89c0010cd8c98cbc48a285262ddeef5d0a198048
Jacob Raaff <103148136+notsilverhoft@users.noreply.github.com>
2024-03-25 04:14:28 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ let is_mobile = /Mobi/i.test(window.navigator.userAgent); if (is_mobile) {
         const canvasElement = document.getElementById('canvas'); let focused = false; return function (ev) {
             if (ev.target == canvasElement) { if (!focused) { canvasElement.focus(); focused = true; } } else { if (focused) { focused = false; } } return true;
         }
-    })()); import { createWaflash } from 'https://cdn.jsdelivr.net/gh/nate-games/waflash@main/waflash-player.min.js';
+    })()); import { createWaflash } from 'https://github.com/notsilverhoft/Waflash-main/blob/master/waflash-player.min.js';
 createWaflash(gameConfig.swfUrl, window.wafOptions || {});
